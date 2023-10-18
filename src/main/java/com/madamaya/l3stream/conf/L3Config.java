2ef10712cf2e21c56b9dafa5f799e91f68fd2480
Masaya YAMADA <yamada-aist@yamada-aist.local>
2023-10-18 15:25:15 +0900

    add L3Config

---
+++ src/main/java/com/madamaya/l3stream/conf/L3Config.java
@@ -0,0 +1,17 @@
+package com.madamaya.l3stream.conf;
+
+// This class was automatically generated.
+// Don't change items manually.
+public class L3Config {
+    public static final String BIN_DIR = "/Users/yamada-aist/workspace/l3stream/bin";
+    public static final String CHECKPOINT_DIR = "/Users/yamada-aist/workspace/l3stream/data/checkpoints";
+    public static final String JAR_PATH = "/Users/yamada-aist/workspace/l3stream/target/l3stream-1.0-SNAPSHOT.jar";
+    public static final String CHECKPOINT_MANAGER_SERVER_IP = "localhost";
+    public static final int CHECKPOINT_MANAGER_SERVER_PORT = 10010;
+    public static final String REDIS_IP = "localhost";
+    public static final int REDIS_PORT = 6379;
+    public static final String FLINK_IP = "localhost";
+    public static final int FLINK_PORT = 8081;
+    public static final String BOOTSTRAP_IP_PORT = "localhost:9092";
+    public static final int PARALLELISM = 4;
+}
