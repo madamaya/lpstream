825ba19ce1e296ac321547d8ebd34ecd638e5f8d
Masaya YAMADA <yamada-aist@yamada-aist.local>
2023-10-20 18:27:21 +0900

    change in/out variables

--- src/main/java/com/madamaya/l3stream/getLineage/TriggerReplay.java
+++ src/main/java/com/madamaya/l3stream/getLineage/TriggerReplay.java
@@ -4,22 +4,23 @@ import com.madamaya.l3stream.conf.L3Config;
 
 public class TriggerReplay {
     public static void main(String[] args) throws Exception {
-        if (args.length != 6) {
+        if (args.length != 7) {
             throw new IllegalArgumentException();
         }
-        String mainPath = args[0];
-        String jobid = args[1];
-        long outputTs = Long.parseLong(args[2]);
-        String lineageTopicName = args[3];
-        long maxWindowSize = Long.parseLong(args[4]);
-        int numOfSource = Integer.parseInt(args[5]);
+        String jarPath = args[0];
+        String mainPath = args[1];
+        String jobid = args[2];
+        long outputTs = Long.parseLong(args[3]);
+        String lineageTopicName = args[4];
+        long maxWindowSize = Long.parseLong(args[5]);
+        int numOfSource = Integer.parseInt(args[6]);
 
         int replayID = FindReplayCPID.getReplayID(outputTs, maxWindowSize, numOfSource);
 
         // Restart
         String replayCommand = L3Config.BIN_DIR + "/templates/lineage.sh";
         System.out.println("Replay from CpID = " + replayID + " of the job (" + jobid + ")");
-        System.out.println("COMMAND --->>> " + replayCommand + " " + L3Config.JAR_PATH + " " + mainPath + " " + jobid + " " + replayID + " " + lineageTopicName);
-        Runtime.getRuntime().exec(replayCommand + " " + L3Config.JAR_PATH + " " + mainPath + " " + jobid + " " + replayID + " " + lineageTopicName);
+        System.out.println("COMMAND --->>> " + replayCommand + " " + jarPath + " " + mainPath + " " + jobid + " " + replayID + " " + lineageTopicName);
+        Runtime.getRuntime().exec(replayCommand + " " + jarPath + " " + mainPath + " " + jobid + " " + replayID + " " + lineageTopicName);
     }
 }
