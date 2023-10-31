26f388685cff1cc9ad41b1c427207ea32d60097a
Masaya YAMADA <yamada-aist@yamada-aist.local>
2023-10-31 17:42:41 +0900

    update dataSizeTest

--- bin/dataSizeTest/dataSizeTest.sh
+++ bin/dataSizeTest/dataSizeTest.sh
@@ -4,6 +4,18 @@ source $(dirname $0)/../config.sh
 
 topic=$1
 checkInterval=$2
+query=$3
+
+#query="LR"
+#query="Nexmark"
+#query="NYC"
+#query="YSB"
+
+mainPath="com.madamaya.l3stream.workflows.${(L)query}.${query}"
+# Run
+echo "*** Run ***"
+echo "(./original.sh ${JAR_PATH} ${mainPath} ${parallelism} metrics1/${query}/baseline 0)"
+./original.sh ${JAR_PATH} ${mainPath} ${parallelism} metrics1/${query}/baseline 0
 
 echo "*** Data size test (topic=${topic}, parallelism=${parallelism}, checkInterval=${checkInterval}) ***"
 echo "java -cp ${JAR_PATH} com.madamaya.l3stream.utils.IdentifyEnd ${topic} ${parallelism} ${checkInterval}"
