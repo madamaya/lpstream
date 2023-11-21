e655bf609376259605ccc7a61ed40402e4a0d283
Masaya YAMADA <yamada-aist@yamada-aist.local>
2023-11-21 18:40:05 +0900

    update bin/utils

---
+++ bin/utils/flinkJob.sh
@@ -0,0 +1,51 @@
+#!/bin/zsh
+
+source $(dirname $0)/../config.sh
+
+# $1: flinkHome
+function cancelFlinkJobs() {
+  echo "get jobid"
+  jobid=(`curl ${flinkIP}:${flinkPort}/jobs | jq '.jobs[] | select( .status == "RESTARTING" or .status == "RUNNING" ) | .id' | xargs echo`)
+  for id in ${jobid[@]}
+  do
+    state=`curl ${flinkIP}:${flinkPort}/jobs/${id} | jq '.state'`
+    if [ ${state} = "RESTARTING" ]; then
+      echo "${state} is restarting (cancelFlinkJobs)."
+    fi
+    echo "********** job info log **********"
+    curl ${flinkIP}:${flinkPort}/jobs/${id}
+    echo "********** job exception log **********"
+    curl ${flinkIP}:${flinkPort}/jobs/${id}/exceptions
+
+    echo "${FLINK_HOME}/bin/flink cancel ${id}"
+    ${FLINK_HOME}/bin/flink cancel ${id}
+  done
+}
+
+function getRunningJobID() {
+  #echo "getRunningJobID"
+  jobid=(`curl ${flinkIP}:${flinkPort}/jobs | jq '.jobs[] | select( .status == "RUNNING" ) | .id' | xargs echo`)
+
+  if [ ${#jobid[*]} -eq 1 ]; then
+    echo ${jobid[1]}
+  else
+    echo "-1"
+  fi
+}
+
+function restartFlinkCluster() {
+  ${FLINK_HOME}/bin/stop-cluster.sh
+  sleep 15
+  ${FLINK_HOME}/bin/start-cluster.sh
+  sleep 15
+}
+
+function restartTMifNeeded() {
+  num=`curl localhost:8081/taskmanagers | jq '.taskmanagers' | jq 'length' | awk '{print $1}'`
+  if [ ${num} -eq 0 ]; then
+    echo "*** no taskmanagers (runningTMnum) ***"
+    echo "*** restart ***"
+    echo "(restartFlinkCluster)"
+    restartFlinkCluster
+  fi
+}
