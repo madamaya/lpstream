62cfb6cfd385c514797ca576b139c1392870737a
Masaya YAMADA <yamada-aist@yamada-aist.local>
2023-11-22 16:16:50 +0900

    add ./bin/dataingest/test.sh

--- bin/dataingest/test.sh
+++ bin/dataingest/test.sh
@@ -1,13 +1,12 @@
 #!/bin/zsh
 
 source $(dirname $0)/../config.sh
-source ../utils/dataIngest.sh
 
-filePath="/Users/yamada-aist/workspace/l3stream/data/input/data/lr.csv.100000"
+filePath="/Users/yamada-aist/workspace/l3stream/data/input/data/lr.csv"
 qName="LR"
 topic="Test"
 throughput=100000
 granularity=1
 
-ingestData ${filePath} ${qName} ${topic} ${parallelism} ${throughput} ${granularity}
-#maxIngestionRate ${filePath} ${qName} ${topic} ${parallelism}
+./ingestData.sh ${filePath} ${qName} ${topic} ${parallelism} ${throughput} ${granularity}
+./maxIngestRate.sh ${filePath} ${qName} ${topic} ${parallelism}
