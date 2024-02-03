34829f59e9955ff5febfbf10f66e59a062e44c0c
Masaya YAMADA <yamada-aist@yamada-aist.local>
2024-02-03 16:38:54 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ if [ $1 = "downloads" ]; then
 
   echo "*** END ***"
 elif [ $1 = "compile" ]; then
-  git clone -b l3streamProcessingLatency git@github.com:madamaya/l3stream-genealog.git
+  git clone -b kafkaProcessingLatency git@github.com:madamaya/l3stream-genealog.git
   python configGenSh2Java.py
   cd l3stream-genealog
   mvn package
