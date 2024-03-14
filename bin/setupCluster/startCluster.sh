6b24bbe83abf089cb88f7687cb6c37858f3f93a8
Masaya YAMADA <yamada-aist@yamada-aist.local>
2024-03-14 15:48:26 +0900

    update setupCluster

--- bin/setupCluster/startCluster.sh
+++ bin/setupCluster/startCluster.sh
@@ -22,39 +22,10 @@ if [[ ${bootstrapServers} == *localhost* ]]; then
 fi
 
 # Create topics
-for i in LR LR2 NYC NYC2 YSB YSB2 Nexmark Nexmark2 Syn1 Syn2 Syn3 Syn4 Syn5 Syn6 Test
+for i in LR NYC NYC2 YSB YSB2 Nexmark Nexmark2 Syn1 Syn2 Syn3
 do
   for j in i o l
   do
     ${KAFKA_HOME}/bin/kafka-topics.sh --create --topic ${i}-${j} --bootstrap-server ${bootstrapServers} --partitions ${parallelism}
   done
 done
-
-<<OUT
-cd ${L3_HOME}
-
-# LR
-echo "*** Ingest LR data into Kafka ***"
-echo "(java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/LinearRoad/lr.csv LR-i)"
-java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/data/lr.csv2 LR-i
-
-# NYC
-echo "*** Ingest NYC data into Kafka ***"
-echo "(java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/NYC/nyc.csv NYC-i)"
-java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/data/nyc.csv NYC-i
-
-# YSB
-echo "*** Ingest YSB data into Kafka ***"
-echo "(java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/YSB/ysb.json YSB-i)"
-java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/data/ysb.json YSB-i
-
-# Nexmark
-echo "*** Ingest Nexmark data into Kafka ***"
-echo "(java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/Nexmark/nexmark.json Nexmark-i)"
-java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/data/nexmark.json Nexmark-i
-
-# Nexmark2
-echo "*** Ingest Nexmark2 data into Kafka ***"
-echo "(java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/Nexmark/nexmark.json Nexmark2-i)"
-java -cp ${JAR_PATH} com.madamaya.l3stream.utils.L3DataLoader ${L3_HOME}/data/input/data/nexmark.json Nexmark2-i
-OUT
