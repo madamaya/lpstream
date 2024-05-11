43c3e1a48d29c660a3e339d3eab93d4cb0d11c32
Masaya YAMADA <yamada-aist@yamada-aist.local>
2024-05-11 13:42:31 +0900

    update lineageReplay.sh

--- bin/templates/lineageReplay.sh
+++ bin/templates/lineageReplay.sh
@@ -6,8 +6,10 @@ source $(dirname $0)/../config.sh
 # $1: jar path, $2: main path, $3: parallelism, $4: jobID, $5: chkID, $6: lineageTopicName, $7: latencyFlag
 if [ ${5} -eq 0 ]; then
 CHK_ARG=""
+startingOffset="earliest"
 else
 CHK_ARG="-s ${L3_HOME}/data/checkpoints/${4}/chk-${5}"
+startingOffset="latest"
 fi
 latencyFlag=2
 aggregateStrategy=""
@@ -28,7 +30,7 @@ ${1} \
 --lineageMode Lineage \
 --lineageTopic ${6} \
 --latencyFlag ${latencyFlag} \
---startingOffset earliest"
+--startingOffset ${startingOffset}"
 
 echo "${EXE_CMD}"
 eval ${EXE_CMD}
